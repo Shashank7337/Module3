716eb00645e73e0531c6cbf1295813a5736bde8b
Shashank shekhar <shashankshekhar7337@gmail.com>
2024-09-16 17:58:20 +0530

    Update Module3.sol

--- Module3.sol
+++ Module3.sol
@@ -1,26 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply, address _owner) ERC20(_name, _symbol) Ownable(_owner) {
-        _mint(_owner, _initialSupply);
+contract ERC20Token is ERC20 , Ownable{
+    constructor() ERC20("Metacrafters", "MT") Ownable(msg.sender) {
+        _mint(msg.sender, 20 * 10 ** decimals()); 
     }
 
-    function mint(address _to, uint256 _value) external onlyOwner returns (bool) {
-        _mint(_to, _value);
+    function mint(address recipient, uint256 amount) external onlyOwner {
+        _mint(recipient, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(from, to, amount);
         return true;
     }
-
-    function burn(uint256 _value) external returns (bool) {
-        _burn(msg.sender, _value);
-        return true;
-    }
-
-function transfer(address _to, uint256 _value) public override returns (bool) {
-    _transfer(_msgSender(), _to, _value);
-    return true;
-}
 }
